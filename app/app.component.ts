97f7b974571f89bf6eff08150900c53865719516
zhouchunyu20xx <zhouchunyu20xx@163.com>
2016-02-22 17:41:51 +0800

    start

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>'
+})
+export class AppComponent { 
+    public title = 'Tour of Heroes';
+		  public hero: Hero = {
+		  id: 1,
+		  name: 'Windstorm'
+		};
+}
+
+interface Hero {
+  id: number;
+  name: string;
+}
